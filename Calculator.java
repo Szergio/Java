f1ae08eafef9ee210b12f493ab959407c3c4fc09
Szergio <46630475+Szergio@users.noreply.github.com>
2022-05-04 17:06:15 -0300

    Calculator 1.2v

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,26 @@
 package start;
 import java.util.Scanner;
 public class Calculator {
+    
+    static void showStart(){
+        System.out.println("0 - Start");
+        System.out.println("1 - Quit");
+        showStart(choose());
+    } 
+    static void showStart(int x){
+        if(x==0 && x!=1){
+            System.out.println("Starting the program ...");
+            System.out.println("");
+            loop();  
+        }
+        else if(x==1){
+            System.out.println("Closing the program ...");
+            System.exit(0);
+        }
+        else{
+            System.out.println("Error showStart(x).");
+        }
+    }
     static int choose(){ 
         Scanner choose = new Scanner(System.in);
         while(true){
@@ -9,93 +29,110 @@ public class Calculator {
             for(int i=0; i < opts.length; i++){     
                 if(opt<opts.length){
                     switch (opt){
-                        case 0:
+                        case 0 -> {
+                            System.out.println("You choose the option 0");
                             return 0;
-                        case 1:
-                            return 1;           
-                        default:
-                            System.out.println("Try again...");
-                            break;
+                        }
+                        case 1 -> {
+                            System.out.println("You choose the option 1");
+                            return 1;
+                        }
+                        default -> System.out.println("Try again ...");
                     }
                 }
                 else if(opts[i]!=opt && opts[i] <= opts.length){
-                    System.out.println("Try again...");
+                    System.out.println("Try again ...");
                     break;
                 }
             }
         }  
-    }                
-
-    static void showStart(){
-        System.out.println("0 - Start");
-        System.out.println("1 - Quit");
-    } 
-    static void showStart(int x){
-        if(x==0){
-          showMath();  
-        }
-        else if(x==1){
-            System.out.println("Bye!");
-            System.exit(0);
-        }
-        else{
-            System.out.println("Error showStart(x).");}
-    }
-    
-    static double numb(){
+    }      
+    static double numbDoubleScanner(){
         Scanner numb = new Scanner(System.in);
         double number = numb.nextDouble();
         return number;
     }
-    
-    static void showMath(){
-        System.out.println("Choose your number:");
+    static int numbIntScanner(){
+        Scanner numb = new Scanner(System.in);
+        int number = numb.nextInt();
+        return number;
     }
-    static void showMath1(){
+    static void showMath(){
+        System.out.println("How many numbers you want for later (+),(-),(*) or (/):");
+    }
+    static double[] listNumber(int amoutsNumb){
+        double[] listNumber = new double[amoutsNumb];
+        System.out.println(amoutsNumb);
+        for(int i=0;i<amoutsNumb;i++){
+            System.out.printf("Insert the %d number:",i+1);
+            System.out.println("");
+            listNumber[i] = numbDoubleScanner();    
+            System.out.println(listNumber);
+        }
+        System.out.println(listNumber);
+        return listNumber;        
+    }
+    static void showMathOperation(){
         System.out.println("0 - addition");
         System.out.println("1 - subtraction");
         System.out.println("2 - multiplication");
         System.out.println("3 - division");
     }
-    static double operation(double a,double b){  
+    static double operation(double[][] list){  
         Scanner choose = new Scanner(System.in);
+        double result = 0;
         while(true){
             int opt = choose.nextInt(); 
             switch (opt){
-            case 0:
-                return a+b;
-            case 1:
-                return a-b;
-            case 2:
-                return a*b;
-            case 3:
-                return a/b;
-            default:
-                System.out.println("Try again...");
+            case 0 -> {
+                for(int i=0;i<list[0].length;i++){
+                    result += list[0][i]; 
+                }
+                return result;
+            }
+            case 1 -> {
+                for(int i=0;i<list[0].length;i++){
+                    result -= list[0][i]; 
+                }
+                return result;
+            }
+            case 2 -> {
+                result = 1;
+                for(int i=0;i<list[0].length;i++){
+                    result *= list[0][i]; 
+                }
+                return result;
+            }
+            case 3 -> {
+                result = list[0][0];
+                for(int i=1;i<list[0].length;i++){
+                    result /= list[0][i]; 
+                }
+                return result;
+            }
+            default -> System.out.println("Try again...");
             } 
         }                
     }
-    static void result(double c){
-        System.out.printf("result = %f",c);
+    
+   
+    
+    static void loop(){
+        double result = 0;
+        double[][] list = new double[100][0];
+        showMath();
+        list[0]=listNumber(numbIntScanner());
+        showMathOperation();
+        result = operation(list);
+  
+        System.out.printf("Result = %.2f",result);
+        System.out.println("");
+        System.out.println("Finished!");
 
     }
-    static void numb(double x){
-        System.out.println("-> "+x);
-    }
-    
     public static void main(String[] args) {
-        System.out.println("Welcome to Calculator 1.1");
+        System.out.println("Welcome to Calculator 1.2");
         showStart();
-        showStart(choose());
-        double a,b,c;
-        a = numb();
-        showMath();
-        b = numb();
-        showMath1();
-        c = operation(a,b);
-        numb(a);
-        numb(b);
-        result(c);
+
     }
-    
 }
